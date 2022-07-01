0840a30ba8e8221757a099b7fd30c9e9e10821c9
Hamid Javadi <javadi.business@gmail.com>
2022-07-01 22:31:13 +0300

    Config mongoose to connect to the database

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   useExpress: true,
   database: {
-    url: 'mongodb://127.0.0.1:27017/',
+    connectionString: 'mongodb://db_admin:123@127.0.0.1:27017/hamidjavadi',
   },
 };
