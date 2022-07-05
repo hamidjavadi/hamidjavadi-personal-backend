cf049f80b151d5e0274dad9fd04ac18663b1d193
Hamid Javadi <javadi.business@gmail.com>
2022-07-05 11:39:17 +0300

    Add MongoDB's test configuration path to tsconfig and jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -89,7 +89,8 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "@utils/(.*)": "<rootDir>/src/utils/$1"
+    "@utils/(.*)": "<rootDir>/src/utils/$1",
+    "@mongoDbTestConfig": "<rootDir>/src/database/mongodb/testConfig.ts"
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
