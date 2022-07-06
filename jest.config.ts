8a878689229a922be7a951d3f1d2d4be063f70a9
Hamid Javadi <javadi.business@gmail.com>
2022-07-06 07:32:00 +0300

    Jest: Config - Set verbose true to show a list of the tests

--- jest.config.ts
+++ jest.config.ts
@@ -89,8 +89,8 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "@utils/(.*)": "<rootDir>/src/utils/$1",
-    "@mongoDbTestConfig": "<rootDir>/src/database/mongodb/testConfig.ts"
+    '@utils/(.*)': '<rootDir>/src/utils/$1',
+    '@mongoDbTestConfig': '<rootDir>/src/database/mongodb/testConfig.ts',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
@@ -190,7 +190,7 @@ export default {
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
+  verbose: true,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
